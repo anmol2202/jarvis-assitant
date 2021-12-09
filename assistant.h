f37e145a008e4c503a24f75d2ec803631ec5b0e2
anmol2202 <anmolgoyal2202@gmail.com>
2021-12-09 17:44:27 +0530

    first commit

---
+++ assistant.h
@@ -0,0 +1,22 @@
+
+
+
+
+#ifndef ASSISTANT_H
+#define ASSISTANT_H
+using namespace std;
+
+class assistant
+{
+
+   public:
+              assistant();
+              ~assistant();
+         void greeting();
+         void typing(string);
+         void clock();
+         void speak(string);
+
+};
+
+#endif // ASSISTANT_H
